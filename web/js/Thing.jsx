0f5f696c41e4b7cff2ec31b4e9e19e686457a615
Mikael Arneborn <mikael@arneborn.net>
2017-05-31 14:28:18 -0700

    web basics

---
+++ web/js/Thing.jsx
@@ -0,0 +1,13 @@
+'use strict';
+
+const React = require('react');
+
+class Thing extends React.Component {
+  render() {
+    return (
+      <div className="thing">A thing</div>
+    );
+  }
+}
+
+module.exports = Thing;
---
+++ web/js/app.jsx
@@ -0,0 +1,10 @@
+'use strict';
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+const Thing = require('./Thing');
+
+ReactDOM.render(
+  <Thing />,
+  document.getElementById('main-container')
+);
