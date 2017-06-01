432931776075b3aa2ba0a79b0a03b2931d67c368
Mikael Arneborn <mikael@arneborn.net>
2017-06-01 14:37:07 -0700

    added mongo to startup

--- scripts/start-consolez-bg.bat
+++ scripts/start-consolez-bg.bat
@@ -4,7 +4,26 @@ echo "Starting jobs that should run forever"
 
 set cmd="%CONSOLEZ%" -c scripts\consoleZ\bg.xml
 
-REM add a mongoDB tab unless mongo's already running.
+REM Make sure we know where various tools live.
+IF NOT DEFINED CONSOLEZ (
+   set CONSOLEZ=C:\Users\Mikael\Programming\Tools\ConsoleZ\Console.exe
+)
+IF NOT DEFINED PYCMDEXE (
+   setx PYCMDEXE %USERPROFILE%\Programming\Tools\PyCmd\PyCmd.exe
+)
+IF NOT DEFINED GRUNTCMD (
+   setx GRUNTCMD %USERPROFILE%\AppData\Roaming\npm\grunt.cmd
+)
+
+REM Make sure %MONGODB% is set
+IF NOT DEFINED MONGODB ( 
+    setx MONGODB=%USERPROFILE%\Programming\mongoDBs\common
+)
+
+IF NOT EXIST %MONGODB% (
+  echo This mongodb doesn't exist: %MONGODB%
+  GOTO END
+)
 
 REM simply add all of these tabs
 set cmd=%cmd% -t server
